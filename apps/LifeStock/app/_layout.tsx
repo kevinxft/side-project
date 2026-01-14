895ee200b667df4db847d32aa4d5b050681c95d9
烂人文 <kevinxft@gmail.com>
2026-01-14 16:41:13 +0800

    🎉 New LifeStock project

---
+++ apps/LifeStock/app/_layout.tsx
@@ -0,0 +1,26 @@
+import { Stack } from 'expo-router';
+import { HeroUINativeProvider } from 'heroui-native';
+import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import 'react-native-reanimated';
+import '../global.css';
+
+export const unstable_settings = {
+  anchor: '(tabs)',
+};
+
+export default function RootLayout() {
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        </Stack>
+
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
+  );
+}
