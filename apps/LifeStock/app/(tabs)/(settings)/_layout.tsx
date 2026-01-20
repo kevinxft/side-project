75322fb421561755323674f7729b7c00067e38ee
烂人文 <kevinxft@gmail.com>
2026-01-20 14:48:08 +0800

    ✨ Setting page

--- apps/LifeStock/app/(tabs)/(settings)/_layout.tsx
+++ apps/LifeStock/app/(tabs)/(settings)/_layout.tsx
@@ -1,6 +1,21 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 import React from "react";
 
 export default function SettingsLayout() {
-    return <Slot />;
+    return (
+        <Stack>
+            <Stack.Screen
+                name="index"
+                options={{
+                    headerShown: false,
+                    headerLargeTitle: true,
+                    headerShadowVisible: false,
+                    headerBlurEffect: "systemMaterial",
+                    headerStyle: {
+                        backgroundColor: "#F2F2F7", // 匹配页面背景色
+                    },
+                }}
+            />
+        </Stack>
+    );
 }
